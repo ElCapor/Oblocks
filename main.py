a75a129612d3f300edd9642ec6e60fa9ab13d458
Sombody <hamdimoubrak@mail.ru>
2022-04-27 00:58:32 +0200

    Templates added

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("catalog"):
+@app.route("/catalog")
 def catalog():
     return render_template("Catalog.html")
 
+@app.route("/admin")
+def admin_catalog():
+    asset1 = {
+        "id":"1",
+        "name":"Pluton Cap",
+        "description" : "Wonder how i got this",
+        "sales" : 100,
+        "thumbnail" : "",
+        "type":"hat",
+        "creator":"pluton"
+    }
+    return render_template("uplaoditem.html", assets=[asset1])
 if __name__ == "__main__":
     app.run()
