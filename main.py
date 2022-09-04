7a90259348f6643376b8ef1c08ba300d0f94f585
Sombody <hamdimoubrak@mail.ru>
2022-09-04 08:22:10 +0200

    nice

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from colorama import Cursor
 from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
-@app.route("/catalog")
-def catalog():
-    return render_template("Catalog.html")
 
+
+def getAllAssets():
+    cursor = sqlite3.connect("catalog.db").cursor()
+    cursor.row_factory = sqlite3.Row
+    results = cursor.execute("Select * from catalog").fetchall()
+    result = [dict(row) for row in results]
+    return result
 @app.route("/admin")
 def admin_catalog():
     asset1 = {
@@ -16,6 +22,23 @@ def admin_catalog():
         "type":"hat",
         "creator":"pluton"
     }
-    return render_template("uplaoditem.html", assets=[asset1])
+    print(getAllAssets())
+    return render_template("uplaoditem.html", assets=[asset1, asset1, asset1, asset1, asset1])
+@app.route("/catalog")
+def catalog():
+    item = {
+        "name" : "Naval Brigade",
+        "price" : "900",
+        "sales" : "190",
+        "favorites" : "9,000",
+        "creator" : "LEgo",
+        "id" : "1"
+    }
+    user = {
+        "name" : "ProPlayer22",
+        "Balance" : 9004,
+        "thumb" : ""
+    }
+    return render_template("Catalog.html", items=[item, item, item, item, item, item], loggedin=user)
 if __name__ == "__main__":
     app.run()
